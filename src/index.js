c312dd393e51ccb6704a2a07a4ddd6cda7ceb9e3
Ахметшин Роберт Данилович <Robert.Akhmetshin@norbit.ru>
2021-07-30 17:28:15 +0300

    Add reducers and devtools extensioin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { compose, createStore } from 'redux';
 import App from './Containers/App'
+import { rootReducer } from './redux/rootReducer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
